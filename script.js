de9c233a299227b60ed14408eec92801a7931f7f
Dr Maxis <axelrousseau935@gmail.com>
2021-09-22 18:19:13 +0200

    Ajout des fichiers index, script et style avec un code de base

---
+++ script.js
@@ -0,0 +1,75 @@
+const root = document.getElementById('root')
+
+alert('Nique!')
+
+
+
+
+/*let name
+let firstName
+let age
+let mailAdress
+let phoneNumber
+
+let productsTotalValue = "154.38";
+let specialOffer  = "10";
+
+let cartTotalValue = productsTotalValue - specialOffer  ;
+console.log(cartTotalValue);
+
+alert('Nique !')
+
+
+{
+    "stadium":{
+        "id" : 18,
+        "label" : "stade de cul",
+        "fields" :[
+        "train1":{
+            "id":1
+            "surface":100
+            "type":"foot"
+    },
+            
+            "train2":{
+                "id":2
+                "surface":200
+                "type":"rugby"
+        }]
+    }
+    
+
+    let termsOfServiceAccepted = false ;
+
+    const boxChecked = () =>{
+        if (termsOfServiceAccepted === true){
+            termsOfServiceAccepted =false
+        }else{
+            termsOfServiceAccepted = true
+        }
+    }
+    
+    <form>
+      <input type="checkbox" value={termsOfServiceAccepted} onChange={boxChecked} />  
+    </form>
+    
+
+
+    const userCredentials = {
+        email : null,
+        mdp : null
+    }
+    
+    const loadEmail = (event) => {
+        userCredentials.email = event.target.value
+    }
+
+    const loadMdp = (event) => {
+        userCredentials.mdp = event.target.value
+    }
+
+    <form>
+        <input type="text" value={userCredentials.email} onChange={loadEmail} />
+        <input type="text" value={userCredentials.mdp} onChange={loadMdp} />
+        <button type="submit">Connecte toi le Jo !</button>
+    </form>*/
