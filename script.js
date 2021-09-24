e7b173ab249632eb4e90d7c1b468b5f517352fbe
Dr Maxis <axelrousseau935@gmail.com>
2021-09-24 20:19:22 +0200

    Creation de l'interface de jeux (template + style)

--- script.js
+++ script.js
@@ -1,86 +1,26 @@
-let totalScorePlayer1
-let currentScorePlayer1
-let totalScorePlayer2
-let currentScorePlayer2
-let diceRoll
-
+let totalScorePlayer1 = 0;
+let currentScorePlayer1 = 0;
+let totalScorePlayer2 = 0;
+let currentScorePlayer2 = 0;
+let diceRoll = 0;
+let playerTurn = 0;
 
 function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addScore() {
+  if (playerTurn == 0) {
+    currentScorePlayer1 += randomInteger(0, 12);
+    document.getElementById("randomScoreValue");
+    document.getElementById("randomScoreValue").innerHTML = currentScorePlayer1;
+    //.setAttribute("value", currentScorePlayer1);
+  } else {
+    currentScorePlayer2 += randomInteger(0, 12);
+    document
+      .getElementById("randomScoreValue")
+      .setAttribute("text", currentScorePlayer2);
   }
+}
 
-document.getElementById("randomScore").addEventListener("click", function() {
-    currentScorePlayer1 = randomInteger(0, 12);
-    document.getElementById("randomScoreValue").setAttribute('value',currentScorePlayer1) ;
-  });
-
-
-
-/*let name
-let firstName
-let age
-let mailAdress
-let phoneNumber
-
-let productsTotalValue = "154.38";
-let specialOffer  = "10";
-
-let cartTotalValue = productsTotalValue - specialOffer  ;
-console.log(cartTotalValue);
-
-alert('Nique !')
-
-
-{
-    "stadium":{
-        "id" : 18,
-        "label" : "stade de cul",
-        "fields" :[
-        "train1":{
-            "id":1
-            "surface":100
-            "type":"foot"
-    },
-            
-            "train2":{
-                "id":2
-                "surface":200
-                "type":"rugby"
-        }]
-    }
-    
-
-    let termsOfServiceAccepted = false ;
-
-    const boxChecked = () =>{
-        if (termsOfServiceAccepted === true){
-            termsOfServiceAccepted =false
-        }else{
-            termsOfServiceAccepted = true
-        }
-    }
-    
-    <form>
-      <input type="checkbox" value={termsOfServiceAccepted} onChange={boxChecked} />  
-    </form>
-    
-
-
-    const userCredentials = {
-        email : null,
-        mdp : null
-    }
-    
-    const loadEmail = (event) => {
-        userCredentials.email = event.target.value
-    }
-
-    const loadMdp = (event) => {
-        userCredentials.mdp = event.target.value
-    }
-
-    <form>
-        <input type="text" value={userCredentials.email} onChange={loadEmail} />
-        <input type="text" value={userCredentials.mdp} onChange={loadMdp} />
-        <button type="submit">Connecte toi le Jo !</button>
-    </form>*/
+document.getElementById("randomScore").addEventListener("click", addScore);
